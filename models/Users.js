221d6250a96e5dd4318075f3d220d7da8998777a
Sandra Martinez <martinez.sand74@gmail.com>
2019-09-24 19:05:20 -0500

    eslint fixes

--- models/Users.js
+++ models/Users.js
@@ -1,12 +1,7 @@
-const bcryptjs = require('bcryptjs');
+var bcryptjs = require("bcryptjs");
 
-module.exports = function (sequelize, DataTypes) {
-
-  const Users = sequelize.define('Users', {
-    id: {
-      type: DataTypes.STRING,
-      unique: true
-    },
+module.exports = function(sequelize, DataTypes) {
+  var Users = sequelize.define("Users", {
     username: {
       type: DataTypes.STRING,
       unique: true
@@ -19,21 +14,25 @@ module.exports = function (sequelize, DataTypes) {
   });
 
   // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  Users.prototype.validPassword = function (password) {
+  Users.prototype.validPassword = function(password) {
     return bcryptjs.compareSync(password, this.password);
   };
 
   // Hooks are automatic methods that run during various phases of the User Model lifecycle
   // In this case, before a User is created, we will automatically hash their password
-  Users.hook("beforeCreate", function (user) {
-    user.username = user.full_name.toLowerCase().replace(/\s/g, '');
-    user.password = bcryptjs.hashSync(user.password, bcryptjs.genSaltSync(10), null);
+  Users.hook("beforeCreate", function(user) {
+    user.username = user.full_name.toLowerCase().replace(/\s/g, "");
+    user.password = bcryptjs.hashSync(
+      user.password,
+      bcryptjs.genSaltSync(10),
+      null
+    );
     console.log(user);
   });
 
-  Users.associate = function (models) {
+  Users.associate = function(models) {
     models.Users.hasMany(models.Posts);
-  }
+  };
 
   return Users;
 };
--- utils/middleware/isAuthenticated.js
+++ utils/middleware/isAuthenticated.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next) {
-    console.log("hi there")
-    if (req.user) {
-      return next();
-    }
-    res.redirect("/");
-  };
+  console.log("hi there");
+  if (req.user) {
+    return next();
+  }
+  res.redirect("/");
+};
